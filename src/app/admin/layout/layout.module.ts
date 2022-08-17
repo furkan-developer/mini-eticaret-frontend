e1eb2335d4959f6987a6ff2090d4f40ac1689fb2
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-17 11:07:10 +0300

    Layout modül oluşturuldu ve Admin modüle declare edildi.

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from './layout/layout.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    LayoutModule
+    CommonModule
   ]
 })
-export class AdminModule { }
+export class LayoutModule { }
