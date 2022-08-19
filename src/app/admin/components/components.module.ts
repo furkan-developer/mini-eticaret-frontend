1fe209baaeb48ef50243bba3b72f3b1cdc3d4290
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-19 15:44:38 +0300

    Specific modules imported (product-customer-orders-dashboard-components)

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders/orders.component';
+import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductsModule,
+    CustomersModule,
+    OrdersModule,
+    DashboardModule
   ]
 })
 export class ComponentsModule { }
