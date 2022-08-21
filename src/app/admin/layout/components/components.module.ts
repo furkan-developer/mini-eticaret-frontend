83e2a35383526eb0280b3b31e1d27df07b8b7781
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-21 12:33:43 +0300

    Added Router Module for sidebar routing

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 
 
@@ -14,7 +14,8 @@ import { RouterModule } from '@angular/router';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent,
