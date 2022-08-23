66ecd8c6afa3ae41362ed7bfe6f295216616d700
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-23 10:18:50 +0300

    AlertifyJS service customized

--- src/app/services/admin/alertify.service.ts
+++ src/app/services/admin/alertify.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+declare var alertify:any;
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,31 @@ import { Injectable } from '@angular/core';
 export class AlertifyService {
 
   constructor() { }
+
+  Message(message:string,messageType:MessageType,position:Position,dismissOthers:Boolean = false,wait:Number = 3){
+    alertify.set('notifier','position', position);
+    const messageObject = alertify[messageType](message,wait);
+    if(dismissOthers)
+      messageObject.dismissOthers();
+  }
+
+  DissmisAll(){
+    alertify.dismissAll();
+  }
+}
+
+export enum MessageType{
+  Error = "error",
+  Message = "message",
+  Success = "success",
+  Warning = "warning"
+}
+
+export enum Position{
+  TopRight = "top-right",
+  TopCenter = "top-center",
+  TopLeft = "top-left",
+  BottomRight = "bottom-right",
+  BottomCenter = "bottom-center",
+  BottomLeft = "bottom-left"
 }
