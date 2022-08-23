4792889c3295636f0a2dd3a6256825dcdd0b6e0b
Furkan Aydin <furkan.aydin@intern.veripark.com>
2022-08-23 09:09:30 +0300

    Created Alertify service

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var alertify:any;
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -12,7 +10,6 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    alertify.success('Success message');
   }
 
 }
